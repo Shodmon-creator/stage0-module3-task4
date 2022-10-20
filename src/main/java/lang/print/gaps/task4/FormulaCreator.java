273a0e7c1ddfc6881b0054baaab9519d01dee822
Shodmon <mamayusupovshodmon2003@gmail.com>
2022-10-20 00:22:07 -0700

    () has been deleted

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -5,6 +5,6 @@ public class FormulaCreator {
     public static void main(String[] args) {
         char a = 'a';
         char b = 'b';
-        System.out.println((9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2));
+        System.out.println((9 * a * a - 5 * b + 2 + a - 7) * (a + b - 4 * a * b) / 2);
     }
 }
